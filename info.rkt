0d800d957963b5ed53025dc7e945f159c1c94768
Masaya Tojo <masaya@tojo.tokyo>
2019-09-28 23:11:06 +0900

    Initial commit

---
+++ info.rkt
@@ -0,0 +1,8 @@
+#lang info
+(define collection "icon")
+(define deps '("base" "htdp-lib"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define scribblings '(("scribblings/icon.scrbl" ())))
+(define pkg-desc "My icon")
+(define version "0.0")
+(define pkg-authors '(tojoqk))
---
+++ scribblings/icon.scrbl
@@ -0,0 +1,10 @@
+#lang scribble/manual
+@require[@for-label[icon
+                    racket/base]]
+
+@title{icon}
+@author{tojo}
+
+@defmodule[icon]
+
+Package Description Here
